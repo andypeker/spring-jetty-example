d238e699c732060fe95039fe2ea7ed7db6331e1f
franklin <andypeker@163.com>
2017-07-02 02:03:26 +0800

    研究了一下MongoDB的连接方式

--- src/main/java/com/fernandospr/example/config/AppConfig.java
+++ src/main/java/com/fernandospr/example/config/AppConfig.java
@@ -1,9 +1,35 @@
 package com.fernandospr.example.config;
 
+import com.mongodb.Mongo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoFactoryBean;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import java.net.UnknownHostException;
 
 @Configuration
 @ComponentScan(basePackages = "com.fernandospr.example")
 public class AppConfig {
+    
+    
+    /*public @Bean
+    Mongo mongo() throws UnknownHostException {
+        return new Mongo("192.168.19.150");
+    }*/
+    
+    /*public @Bean
+    MongoFactoryBean mongo() {
+        MongoFactoryBean mongo = new MongoFactoryBean();
+        mongo.setHost( "localhost" );
+        return mongo;
+    }*/
+    
+    /*public @Bean
+    MongoDbFactory mongoDbFactory() throws Exception {
+        return new SimpleMongoDbFactory( new Mongo(), "database" );
+    }*/
+    
 }
--- src/main/java/com/fernandospr/example/config/MongoConfig.java
+++ src/main/java/com/fernandospr/example/config/MongoConfig.java
@@ -1,19 +1,43 @@
 package com.fernandospr.example.config;
 
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class MongoConfig {
 
-    private static final String DB_NAME = "example";
+    private static final String DB_NAME = "admin";
 
     public @Bean MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(), DB_NAME);
+//        return new SimpleMongoDbFactory(new MongoClient(), DB_NAME);
+    
+        /***
+         * 这里应该配置一个mongoDB的Host和Ip作为参数，可能如下：
+         * return new SimpleMongoDbFactory(new MongoClient('192.168.19.150', 12321), 'frankie');
+         * **/
+        ServerAddress svrAddr = new ServerAddress("192.168.19.150", 27017);
+        MongoCredential mongoCred = MongoCredential.createCredential("frankie", DB_NAME, "135317".toCharArray());
+        List<MongoCredential> mongoCreds = new ArrayList<>(Arrays.asList(mongoCred));
+        MongoClient mongoClt = new MongoClient(svrAddr, mongoCreds);
+        return new SimpleMongoDbFactory(mongoClt, DB_NAME);
+        
+        /******
+         * 上下二个连接MongoDB的方式，经过验证都OK
+         * ****/
+    
+//        MongoClientOptions.Builder buldr = new MongoClientOptions.Builder();
+//        MongoClientURI mongoCURI = new MongoClientURI("mongodb://frankie:135317@192.168.19.150:27017/admin", buldr);
+//        return new SimpleMongoDbFactory(mongoCURI);
+        
     }
 
     public @Bean MongoTemplate mongoTemplate() throws Exception {
